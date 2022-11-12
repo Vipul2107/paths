73a88ec00c7be5bb27f357dbbbb0be96a08a37eb
Vipul2107 <82260540+Vipul2107@users.noreply.github.com>
2022-11-12 08:07:36 +0530

    Add files via upload

---
+++ src/algorithms/breadthFirstSearch.js
@@ -0,0 +1,35 @@
+import { findNeighbors } from '../config';
+import { bestfirstsearch } from './bestFirstSearch';
+
+const breadthFirstSearch = (grid, start, finish) => {
+  const visited = [start];
+  const queue = [start];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    const destinations = findNeighbors(grid, node.row, node.col);
+    for (const destination of destinations) {
+      if (destination === finish) {
+        destination.isVisited = true;
+        destination.previousNode = node;
+        visited.push(destination)
+        return visited;
+      }
+
+      if (!destination.isVisited && !destination.isWall) {
+        destination.isVisited = true;
+        visited.push(destination);
+        queue.push(destination);
+
+        if (destination !== start) {
+          destination.previousNode = node;
+        }
+      }
+    }
+  }
+}
+
+export {
+  breadthFirstSearch,
+}
+
